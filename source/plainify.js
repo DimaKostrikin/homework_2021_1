f444198ee3c8befbd8e21e0060d9b98f5a2512c2
Dmitry Kostrikin <dima.da222@yandex.ru>
2021-02-12 23:18:13 +0300

    3

--- source/plainify.js
+++ source/plainify.js
@@ -3,17 +3,15 @@
 /**
  * Plainifies a nested object.
  * @author Dmitry Kostrikin
- * @constructor
- * @version 0.0.1
- * 
- * @example <caption>Example usage</caption>
- * plainify(nested);
  * @param {Object} nested
  * @returns {Object|null} If sent param is not an object, returns null
  */
 
+const isValidObject = variable =>
+	Object.prototype.toString.call(variable) === '[object Object]'
+
 const plainify = (nested, keyPath = '') =>
-	nested.constructor === Object ?
+	isValidObject(nested) ?
 	Object.entries(nested).reduce((plainObject, [ key, value ]) => {
 		const newKeyPath = `${keyPath}${keyPath ? '.' : ''}${key}`;
 		return Object.assign(plainObject, value.constructor === Object
