daf39dfa3df9b05d75a4b077cdcdc33936e6e6db
Dmitry Kostrikin <dima.da222@yandex.ru>
2021-02-13 20:35:14 +0300

    jsdoc fixed

--- source/plainify.js
+++ source/plainify.js
@@ -1,14 +1,21 @@
 'use strict';
 
 /**
+ * Checks if the variable is valid. 
+ * @author Dmitry Kostrikin
+ * @param {Object} variable
+ * @returns {true|false} If sent variable is not an object, returns false.
+ */
+
+const isValidObject = variable =>
+	Object.prototype.toString.call(variable) === '[object Object]'
+
+/**
  * Plainifies a nested object.
  * @author Dmitry Kostrikin
  * @param {Object} nested
  * @returns {Object|null} If sent param is not an object, returns null
  */
-
-const isValidObject = variable =>
-	Object.prototype.toString.call(variable) === '[object Object]'
 
 const plainify = (nested, keyPath = '') =>
 	isValidObject(nested) ?
